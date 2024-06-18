188b36e14f35bd70e2a7b3f5e53b4260f40a83fa
Dmitrey Makarov-Paton <dmitrey.makarov-paton@myob.com>
2024-06-18 16:05:37 +1200

    Added T240 source files from https://github.com/Acumatica/Help-and-Training-Examples/tree/2024R1/Customization/T240

--- OpenVS.cmd
+++ OpenVS.cmd
@@ -1,5 +1,5 @@
-﻿@echo off
+@echo off
 setlocal
-set ACC_SITE_PATH=C:\src\temp\AcumaticaERPInstall_ 24.191.0155P\PhoneRepairShop
+set ACC_SITE_PATH=C:\src\temp\AcumaticaERPInstall_24.191.0155P\PhoneRepairShop
 echo ---------------
 start /B "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.exe" "%~dp0\PhoneRepairShop.sln %*"
