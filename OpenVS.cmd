cdcdb4b39942cbdc03a806f567540695be94cbd2
Dmitrey Makarov-Paton <dmitrey.makarov-paton@myob.com>
2024-06-18 15:26:19 +1200

    Setup: Added path to the app

--- OpenVS.cmd
+++ OpenVS.cmd
@@ -1,5 +1,5 @@
 ﻿@echo off
 setlocal
-set ACC_SITE_PATH=C:\src\temp\AcumaticaERPInstall_23.23.206.0006\Apps
+set ACC_SITE_PATH=C:\src\temp\AcumaticaERPInstall_ 24.191.0155P\PhoneRepairShop
 echo ---------------
 start /B "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.exe" "%~dp0\PhoneRepairShop.sln %*"
