a8a423971bfaf3af3293824a8e7dc0b6ca7b8982
Dmitrey Makarov-Paton <dmitrey.makarov-paton@myob.com>
2024-06-18 15:17:13 +1200

    Init checkin

---
+++ OpenVS.cmd
@@ -0,0 +1,5 @@
+﻿@echo off
+setlocal
+set ACC_SITE_PATH=C:\src\temp\AcumaticaERPInstall_23.23.206.0006\Apps
+echo ---------------
+start /B "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.exe" "%~dp0\PhoneRepairShop.sln %*"
